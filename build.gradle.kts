77afe0e97065c295a0fb6a1050eaa3467c67f822
Galo Ortega <galoefren@hotmail.com>
2026-02-23 16:38:55 -0500

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.11-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.quark"
+version = "0.0.1-SNAPSHOT"
+description = "api gateway demo"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+extra["springCloudVersion"] = "2025.0.1"
+
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
