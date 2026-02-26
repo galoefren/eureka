86c3e9540ff6855769552afdc96632c94123e431
Galo Ortega <galoefren@hotmail.com>
2026-02-26 12:22:11 -0500

    First commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 extra["springCloudVersion"] = "2025.0.1"
 
 dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
